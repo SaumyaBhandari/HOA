3ecc95dcb58d8589765e564cd80ae3af4afb78bf
SaumyaBhandari <np03a190011@heraldcollege.edu.np>
2020-05-24 23:14:50 +0545

    Updates

---
+++ precache-manifest.0901e03287304f2ca1ed38ee099d116e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850981998b99877f635b2428e6c72c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693873b2d5e85289d986",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "3873c433450d956e8ae6",
+    "url": "/static/css/main.243c6e6f.chunk.css"
+  },
+  {
+    "revision": "693873b2d5e85289d986",
+    "url": "/static/js/2.1a028b49.chunk.js"
+  },
+  {
+    "revision": "bc4f78fb421f28a294116f2bc1b0832a",
+    "url": "/static/js/2.1a028b49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3873c433450d956e8ae6",
+    "url": "/static/js/main.a7fc1647.chunk.js"
+  },
+  {
+    "revision": "c516647b492b96aa2e76",
+    "url": "/static/js/runtime-main.80e2530a.js"
+  },
+  {
+    "revision": "17dac1a7593db0ecd61c45bc79882833",
+    "url": "/static/media/background.17dac1a7.jpg"
+  },
+  {
+    "revision": "b03a24b2ab761e4133cd4a30f277c076",
+    "url": "/static/media/breaker.b03a24b2.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
